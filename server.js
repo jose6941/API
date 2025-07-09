3d68c650bc595df425669195b0fb704f364dc796
Jose6941 <jose.souza1@aluno.ifsp.edu.br>
2025-07-09 18:36:03 -0300

    .

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+import express from "express";
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
 
-const api = require('./backend/routes');
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT;
+const URI = process.env.MONGO_URL_CONNECT;
 
-app.use(bodyParser.json());
+const connectDB = async () => {
+    try{
+        await mongoose.connect(URI);
+        console.log("Conectado ao MongoDB");
+    }catch(error){
+        console.log("Erro ao conectar ao MongoDB");
+    }
+};
+
+connectDB();
 
 app.get('/', (req,res) => {
-    res.json({
-        sucecess: true
-    });
+
 });
 
-app.use('/api', api)
-
-const PORT = process.env.PORT;
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+})
